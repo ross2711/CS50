22de11f1e1c49667faeef555e3e4ced4c770be4c
ross2711 <30591659+ross2711@users.noreply.github.com>
2017-10-12 21:01:00 +0200

    hello.c
    Implement a program that prints out a simple greeting to the user as 
    per CS50 specification.

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*
+
+Implement a program that prints out a simple greeting to the user as 
+per CS50 specification.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
